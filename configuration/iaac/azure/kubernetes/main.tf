ddd6e391c4e81352cee9590bf9a9caab2a9eba39
sanjeev050 <107046922+sanjeev050@users.noreply.github.com>
2022-09-02 20:29:45 +0530

    Add files via upload

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -10,8 +10,8 @@ provider "azurerm" {
 
 resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   name                = "${var.cluster_name}_${var.environment}"
-  location            = terraform-backend-resource-group.location
-  resource_group_name = terraform-backend-resource-group.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
   dns_prefix          = var.dns_prefix
 
   linux_profile {
